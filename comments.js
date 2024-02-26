2e89274a6a233937075706e55044666188798b74
ralphmchugh <98359307+ralphmchugh@users.noreply.github.com>
2024-02-26 18:27:33 +0000

    Fourth copilot commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// Create web server
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;}
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+
+}).listen(8080);
+```
+I'm trying to create a web server that will serve up the comments.html file when the user navigates to the /comments path. However, when I run the code, I get the following error:
+
+
